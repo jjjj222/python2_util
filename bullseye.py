e786ab5191dba5a430c64706b85245ff22728653
jjjj222 <jjjj222.tso@gmail.com>
2018-05-23 02:15:40 -0700

    Use single self.ostream in bullseye

--- bullseye.py
+++ bullseye.py
@@ -15,8 +15,6 @@ class Bullseye:
     def __init__(self, build_zone):
         self.build_zone = build_zone
         self.cov_file = os.path.join(self.build_zone, "bullseye.cov")
-        #self.region_file = self.cov_file + ".region"
-        #self.region_file = None
         self.html_folder = self.cov_file + ".html"
         self.csv_file = self.cov_file + ".csv"
         self.bullshtml_folder = self.cov_file + ".bullshtml"
@@ -24,6 +22,9 @@ class Bullseye:
         self.bin = None
         self.bullshtml_jar = None
 
+        self.ostream = sys.stdout
+
+#-------------------------------------------------------------------------------
     @property
     def cov01(self):
         return os.path.join(self.bin, "cov01")
@@ -44,45 +45,43 @@ class Bullseye:
     def covxml(self):
         return os.path.join(self.bin, "covxml")
 
+#-------------------------------------------------------------------------------
     def on(self):
         cmd = self.cov01 + " --on"
+
         env = os.environ.copy()
         env["COVFILE"] = self.cov_file
         env["COVBUILDZONE"] = self.build_zone
         show_env = ["COVFILE", "COVBUILDZONE"]
 
-        ostream = sys.stdout
-        cmd_util.run_cmd_oneline(ostream, cmd, None, env, show_env)
+        cmd_util.run_cmd_oneline(self.ostream, cmd, None, env, show_env)
 
     def off(self):
         cmd = self.cov01 + " --off"
+
         env = os.environ.copy()
         env["COVFILE"] = self.cov_file
         env["COVBUILDZONE"] = self.build_zone
         show_env = ["COVFILE", "COVBUILDZONE"]
 
-        ostream = sys.stdout
-        cmd_util.run_cmd_oneline(ostream, cmd, None, env, show_env)
+        cmd_util.run_cmd_oneline(self.ostream, cmd, None, env, show_env)
 
     def apply_region(self, region_file):
         if not region_file:
             return
 
-        #cmd = self.covselect + " --file " + self.cov_file + " --import " + self.region_file
         cmd = self.covselect + " --file " + self.cov_file + " --import " + region_file
 
         show_env = []
 
-        ostream = sys.stdout
-        cmd_util.run_cmd_oneline(ostream, cmd, None, None, show_env)
+        cmd_util.run_cmd_oneline(self.ostream, cmd, None, None, show_env)
 
     def generate_html(self):
         cmd = self.covhtml + " --file " + self.cov_file + " " + self.html_folder
 
         show_env = []
 
-        ostream = sys.stdout
-        cmd_util.run_cmd_oneline(ostream, cmd, None, None, show_env)
+        cmd_util.run_cmd_oneline(self.ostream, cmd, None, None, show_env)
 
     def generate_csv(self):
         cmd = self.covsrc + " --file " + self.cov_file + " --csv --quiet"
@@ -90,7 +89,8 @@ class Bullseye:
 
         show_env = []
 
-        print "Create bullseye result csv file:", self.csv_file
+        self.ostream.write("Create bullseye result csv file: %s\n" % (self.csv_file))
+
         ostream = open(self.csv_file, 'w')
         cmd_util.run_cmd_no_progress(ostream, cmd=cmd, cwd=cwd)
 
@@ -99,8 +99,4 @@ class Bullseye:
 
         show_env = []
 
-        ostream = sys.stdout
-        cmd_util.run_cmd_oneline(ostream, cmd, None, None, show_env)
-
-    #def remove_cov_file(self):
-    #    file_util.removeFile(self.cov_file)
+        cmd_util.run_cmd_oneline(self.ostream, cmd, None, None, show_env)
